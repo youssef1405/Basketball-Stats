10e0e49e225c08f521771efe87ee71e36d6e7f42
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-10-30 17:43:21 -0400

    split gaudians into a list

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import constants
 
+PLAYERS = constants.PLAYERS
+TEAMS = constants.TEAMS
+Panther = []
+Bandits = []
+Warriors = []
+
+
+# print(len(PLAYERS)) 18
+
 def clean_data(PLAYERS):
     players = []
 
     for player in PLAYERS:
         new_player = {}
         new_player['name'] = player['name']
-        new_player['guardians'] = player['guardians']
+        new_player['guardians'] = player['guardians'].split('and')
         if player['experience'] == 'YES':
             new_player['experience'] = True
         else:
@@ -14,4 +23,10 @@ def clean_data(PLAYERS):
         new_player['height'] = int(player['height'].split(' ')[0])
         players.append(new_player)
     return players
+
+# def balance_teams():
+
+
+if __name__ == '__main__':
+    print(clean_data(PLAYERS)[:2])
 
