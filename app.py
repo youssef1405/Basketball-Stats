403a697ef782523aefe0c193ee8a376a07702a6f
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-10-30 20:15:08 -0400

    distribute players to the three teams

--- app.py
+++ app.py
@@ -1,32 +1,56 @@
 import constants
 
-PLAYERS = constants.PLAYERS
-TEAMS = constants.TEAMS
-Panther = []
-Bandits = []
-Warriors = []
+players = constants.PLAYERS
+teams = constants.TEAMS
+panther = []
+bandits = []
+warriors = []
+experinced_players = []
+non_experienced_players = []
 
-
-# print(len(PLAYERS)) 18
 
 def clean_data(PLAYERS):
-    players = []
 
-    for player in PLAYERS:
+    for player in players:
         new_player = {}
         new_player['name'] = player['name']
         new_player['guardians'] = player['guardians'].split('and')
+        new_player['height'] = int(player['height'].split(' ')[0])
         if player['experience'] == 'YES':
             new_player['experience'] = True
+            experinced_players.append(new_player)
         else:
             new_player['experience'] = False 
-        new_player['height'] = int(player['height'].split(' ')[0])
-        players.append(new_player)
-    return players
+            non_experienced_players.append(new_player)
+    return experinced_players, non_experienced_players
 
-# def balance_teams():
+def balance_teams():
+    players_per_team = len(players) // len(teams)
+    exp_players_per_team = len(experinced_players) // len(teams)
+
+    for exp_player in experinced_players:
+        if len(panther) < exp_players_per_team:
+            panther.append(exp_player)
+        elif len(warriors) < exp_players_per_team:
+            warriors.append(exp_player)
+        else:
+            bandits.append(exp_player)
+    
+    for non_exp_player in non_experienced_players:
+        if len(panther) < players_per_team:
+            panther.append(non_exp_player)
+        elif len(warriors) < players_per_team:
+            warriors.append(non_exp_player)
+        else:
+            bandits.append(non_exp_player)
+
 
 
 if __name__ == '__main__':
-    print(clean_data(PLAYERS)[:2])
+    clean_data(players)
+    balance_teams()
+    print(len(warriors))
+    for player in warriors:
+        print(player)
+
 
