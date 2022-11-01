572fe84f9ed59605938f4303ea9c40f1a4af4b3d
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-10-31 22:40:48 -0400

    print team stats on the screen

--- app.py
+++ app.py
@@ -45,19 +45,58 @@ def balance_teams():
             bandits.append(non_exp_player)
 
 
-def print_status():
+def print_menu():
     print('------ MENU ------\n')
     print('Here are your choices:1\n1) Display Teams Stats\n2) Quit\n')
-    option_1 = input('Enter an option: ') 
-    print('1) Panthers\n2) Bandits\n3) Warriors')
-    option_2 = input('Enter an option: ')
+    option_1 = int(input('Enter an option: '))
+    if option_1 == 2:
+        return
+    elif option_1 == 1: 
+        print('1) Panthers\n2) Bandits\n3) Warriors')
+        option_2 = int(input('Enter an option: \n'))
+        if option_2 == 1:
+            print_stats(panther, 'Panthers')
+        elif option_2 == 2:
+            print_stats(bandits, 'Bandits')
+        elif option_2 == 3:
+            print_stats(warriors, 'Warriors')
+
+def print_stats(team, team_name):
+    total_experienced = 0
+    total_inexperienced = 0
+    avg_height = 0
+    total_height = 0
+    players_names = ''
+    guardians =''
+
+    for index, player in enumerate(team):
+        total_height += player['height']
+        if index < len(team) - 1:
+            players_names += (player['name']).strip() + ', '
+            guardians += ', '.join(map(str.strip, player['guardians'])) + ', '
+        else:
+            players_names += player['name']
+            guardians += ', '.join(map(str.strip, player['guardians']))
+        if player['experience']:
+            total_experienced += 1
+        else:
+            total_inexperienced += 1
+
+    avg_height = total_height / len(team)
+
+    print(f'Team {team_name} Stats')
+    print('-----------------------')
+    print(f'Total players: {len(team)}')
+    print(f'Total experienced: {total_experienced}')
+    print(f'Total inexperienced: {total_inexperienced}')
+    print(f'Average height: {avg_height}\n')
+    
+    print(f'Players on Team: \n{players_names}\n')
+    print(f'Guardians:\n{guardians}')
 
 if __name__ == '__main__':
-    print_status()
     clean_data(players)
     balance_teams()
-    print(len(warriors))
-    for player in warriors:
-        print(player)
+    print_menu()
 
 
